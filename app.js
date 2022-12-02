08c2d688d76700fcb6049bc02bf744c525f76538
gary909 <garywhite61@hotmail.com>
2022-12-02 10:55:40 +0100

    code

---
+++ app.js
@@ -0,0 +1,33 @@
+function getLength(arr){
+    //return length of arr
+    return ?
+}
+
+function getFirst(arr){
+    //return the first element of arr
+    return ?
+}
+  
+function getLast(arr){
+    //return the last element of arr
+    return ?
+}
+
+function pushElement(arr){
+    var el=1;
+    //push el to arr
+    
+    return arr
+}
+ 
+function popElement(arr){
+    //pop an element from arr
+    
+    return ?
+}
+
+console.log(getLength([1,2,3]));// 3
+console.log(getFirst([1,2,3]));// 1
+console.log(getLast([1,2,3]));// 3
+console.log(pushElement([1,2,3])); // 4
+console.log(popElement([1,2,3])); // 4
